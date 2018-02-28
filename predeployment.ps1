3dd2d6c0a480ddb09a9a35aaeb23b7dfdfb71858
khatru734 <33819082+khatru734@users.noreply.github.com>
2018-02-27 21:26:39 -0600

    Launch in PowerShell 2
    Launches pre-deployment script in PowerShell 2 to simulate running in a Windows 7 environment.

--- predeployment.ps1
+++ predeployment.ps1
@@ -1,3 +1,7 @@
+#Start PowerShell Version 2.0
+Write-Host "Starting PowerShell Version 2..."
+powershell.exe -version 2
+Start-Sleep -s 1
 #Enumerate which drive letter the deployment stick is.
 #
 Write-Host "Beginning Data Gathering..."
